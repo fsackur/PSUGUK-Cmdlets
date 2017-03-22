e28d0778b8da7760ceb4e3740daa39d5042ea74b
fsackur <freddie.sackur@hotmail.com>
2017-03-22 23:19:47 +0000

    Added PLatyPS support (Open PowerShell x86 and run: Install-Module PlatyPS)

---
+++ PSUGUK-Cmdlets/PostBuild.ps1
@@ -0,0 +1,33 @@
+﻿<#
+    .Synopsis
+    TeamVillage
+
+    .Description
+    Run as a post-build event
+#>
+param(
+	#the full path to the project .dll file
+    [string]$TargetPath
+)
+
+
+$ProjectDir = $PSScriptRoot
+$TargetDir = Split-Path $TargetPath
+Set-Location $TargetDir
+$ModuleName = (Split-Path $TargetPath -Leaf) -replace '\.dll$'
+
+
+#region Update help file
+Import-Module PlatyPS          #Install-Module PlatyPS (run in PowerShell x86 because, hey, Visual Studio)
+Import-Module $TargetPath
+$Cmdlets = Get-Command -Module $ModuleName -CommandType Cmdlet
+
+#If the help markdown isn't there, create it
+New-MarkdownHelp -Command $Cmdlets -OutputFolder $ProjectDir\docs -ErrorAction SilentlyContinue
+
+#if the help markdown is there, update it with any changes to the parameter bindings
+Update-MarkdownHelp -Path $ProjectDir\docs
+[void](New-Item -ItemType Directory -Path en-US -Force)
+New-ExternalHelp -Path $ProjectDir\docs -OutputPath .\en-US -Force
+
+#endregion Update help file

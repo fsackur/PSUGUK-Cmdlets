dafe2da8fb5028c88fc1a95a9a71534170b4b65a
fsackur <freddie.sackur@hotmail.com>
2017-03-22 22:41:31 +0000

    Added cmdlet to create DnsResolver; handles input robustly

--- PSUGUK-Cmdlets/Debug-Project.ps1
+++ PSUGUK-Cmdlets/Debug-Project.ps1
@@ -2,16 +2,33 @@
 $Host.UI.RawUI.WindowTitle = 'Debugging'
 Import-Module .\Dusty.AdConnectivity.dll
 
- $CorpDomain = 'corp.dustyfox.uk'
- $CorpIp = '134.213.29.116'
- 
+$CorpDomain = 'corp.dustyfox.uk'
+$CorpIp = '134.213.29.116'
 
- #==============================================#
- 
- Write-Host '    $R = New-Object Dusty.ADConnectivity.DnsResolver $CorpIp
- '
- $R = New-Object Dusty.ADConnectivity.DnsResolver $CorpIp
 
- Write-Host '    $R.Query($CorpDomain)
- '
- $R.Query($CorpDomain)
+#==============================================#
+
+
+Write-Host -ForegroundColor Magenta '
+$DustyDC = Get-DnsResolver $CorpDomain
+$DustyDC
+'
+$DustyDC = Get-DnsResolver $CorpDomain
+$DustyDC | Out-String
+
+Write-Host -ForegroundColor Magenta '
+$Goog = "8.8.8.8" | Get-DnsResolver
+$Goog
+'
+$Goog = "8.8.8.8" | Get-DnsResolver
+$Goog | Out-String
+
+Write-Host -ForegroundColor Magenta '
+"8.8.8.8", $CorpDomain | Get-DnsResolver
+'
+"8.8.8.8", $CorpDomain | Get-DnsResolver | Out-String
+
+Write-Host -ForegroundColor Magenta '
+Get-DnsResolver "8.8.8.8", $CorpDomain
+'
+Get-DnsResolver "8.8.8.8", $CorpDomain | Out-String
---
+++ PSUGUK-Cmdlets/PSCmdlets/GetDnsResolver.cs
@@ -0,0 +1,55 @@
+﻿//https://www.codeproject.com/Articles/23673/DNS-NET-Resolver-C
+//Install-Package Heijden.DNS
+using Heijden.DNS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using System.Net;
+using System.Text;
+using Dusty.ADConnectivity;
+using System.Management;
+
+namespace Dusty.ADConnectivity
+{
+
+    [Cmdlet(VerbsCommon.Get, "DnsResolver")]
+    [OutputType(typeof(DnsResolver))]
+    public class GetDnsResolver : PSCmdlet
+    {
+        //for ease of use, we want to accept hostnames or ipaddresses
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        public string[] DnsServer { get; set; }
+
+
+        protected override void ProcessRecord()
+        {
+            
+            foreach (string name in DnsServer)
+            {
+                if (Uri.CheckHostName(name.ToString()) == UriHostNameType.IPv4 ||
+                        Uri.CheckHostName(name.ToString()) == UriHostNameType.IPv6)
+                {
+                    IPAddress ip = IPAddress.Parse(name);
+                    WriteObject(new DnsResolver(new IPEndPoint(ip, 53)));
+                    continue;
+                }
+
+                try
+                {
+                    IPHostEntry host = Dns.GetHostEntry(name);
+                    foreach (IPAddress ip in host.AddressList)
+                    {
+                        WriteObject(new DnsResolver(new IPEndPoint(ip, 53)));
+                    }
+                }
+                catch
+                {
+                    WriteWarning(string.Format("Unable to resolve hostname {0}", name));
+                }
+
+            } //end foreach name in DnsServer
+        }  //end ProcessRecord
+
+    }
+}

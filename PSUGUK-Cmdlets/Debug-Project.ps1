2cbb336be2e9f9b82295e2c759a9bf84e04d5ef1
fsackur <freddie.sackur@hotmail.com>
2017-03-22 21:52:09 +0000

    Taking it back to the beginning; time to get SIRRYUS

--- PSUGUK-Cmdlets/Debug-Project.ps1
+++ PSUGUK-Cmdlets/Debug-Project.ps1
@@ -2,11 +2,3 @@
 $Host.UI.RawUI.WindowTitle = 'Debugging'
 Import-Module .\Dusty.AdConnectivity.dll
 
-
-
-Write-Host -ForegroundColor Yellow 'By parameter:'
-Get-Stupider 'Jim', 'Bob' | Out-String
-
-
-Write-Host -ForegroundColor Yellow 'From pipeline: '
-'Zinnie', 'Lou-Lou' | Get-Stupider | Out-String

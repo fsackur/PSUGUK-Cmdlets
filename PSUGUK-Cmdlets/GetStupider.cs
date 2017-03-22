f081f3b235d9741038d036d3b3a2f28398a872a9
fsackur <freddie.sackur@hotmail.com>
2017-03-22 01:10:03 +0000

    Added first cmdlet

---
+++ PSUGUK-Cmdlets/GetStupider.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Dusty.AdConnectivity
+{
+    /*
+     * Won't compile - needs a reference to the System.Management.Automation assembly
+     * 
+     * Install-Package System.Management.Automation_PowerShell_3.0
+     * 
+     */ 
+    [Cmdlet("Get", "Stupider")]
+    class GetStupider : PSCmdlet
+    {
+
+    }
+}

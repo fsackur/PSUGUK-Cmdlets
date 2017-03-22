e10c3edcc009461456c7499a47d6f8dd4c7b9747
fsackur <freddie.sackur@hotmail.com>
2017-03-22 22:54:42 +0000

    we must look to another direction for help

--- PSUGUK-Cmdlets/Debug-Project.ps1
+++ PSUGUK-Cmdlets/Debug-Project.ps1
@@ -10,25 +10,6 @@ $CorpIp = '134.213.29.116'
 
 
 Write-Host -ForegroundColor Magenta '
-$DustyDC = Get-DnsResolver $CorpDomain -Verbose
-$DustyDC
+Get-Help Get-DnsResolver -Full
 '
-$DustyDC = Get-DnsResolver $CorpDomain -Verbose
-$DustyDC | Out-String
-
-Write-Host -ForegroundColor Magenta '
-$Goog = "8.8.8.8" | Get-DnsResolver -Verbose
-$Goog
-'
-$Goog = "8.8.8.8" | Get-DnsResolver -Verbose
-$Goog | Out-String
-
-Write-Host -ForegroundColor Magenta '
-"8.8.8.8", $CorpDomain | Get-DnsResolver -Verbose
-'
-"8.8.8.8", $CorpDomain | Get-DnsResolver  -Verbose | Out-String
-
-Write-Host -ForegroundColor Magenta '
-Get-DnsResolver "8.8.8.8", $CorpDomain -Verbose
-'
-Get-DnsResolver "8.8.8.8", $CorpDomain  -Verbose | Out-String
+Get-Help Get-DnsResolver -Full

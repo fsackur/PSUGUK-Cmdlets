1861a3166bf2288756f42ef961c185094ef9452f
fsackur <freddie.sackur@hotmail.com>
2017-03-22 20:55:55 +0000

    Working with pipeline input

---
+++ PSUGUK-Cmdlets/Debug-Project.ps1
@@ -0,0 +1,12 @@
+﻿
+$Host.UI.RawUI.WindowTitle = 'Debugging'
+Import-Module .\Dusty.AdConnectivity.dll
+
+
+
+Write-Host -ForegroundColor Yellow 'By parameter:'
+Get-Stupider 'Jim', 'Bob'
+
+Write-Host ""
+Write-Host -ForegroundColor Yellow 'From pipeline: '
+'Zinnie', 'Lou-Lou' | Get-Stupider
--- PSUGUK-Cmdlets/GetStupider.cs
+++ PSUGUK-Cmdlets/GetStupider.cs
@@ -10,18 +10,13 @@ namespace Dusty.AdConnectivity
     [Cmdlet(VerbsCommon.Get, "Stupider")]
     public class GetStupider : PSCmdlet
     {
-        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, Position = 0)] //, ValueFromPipeline = true)]
         public string[] StupidName { get; set; }
-
 
         protected override void BeginProcessing()
         {
-            WriteObject(string.Format(
-                        "{0} {1} stupid.",
-                        string.Join(" and ", StupidName),
-                        StupidName.Length > 1 ? "is" : "are"
-                    )
-                );
+            //Parameters that accept pipeline input are not initialised in the begin block
+            WriteObject(StupidName);
         }
 
         protected override void ProcessRecord()

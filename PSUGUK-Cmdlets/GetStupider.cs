90d3837fbcf5b98dd834807f473851faf4c0cc74
fsackur <freddie.sackur@hotmail.com>
2017-03-22 01:11:43 +0000

    Added ref to system.management.automation.dll so it will compile

--- PSUGUK-Cmdlets/GetStupider.cs
+++ PSUGUK-Cmdlets/GetStupider.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Automation;
 using System.Text;
 
 namespace Dusty.AdConnectivity
 {
-    /*
-     * Won't compile - needs a reference to the System.Management.Automation assembly
-     * 
-     * Install-Package System.Management.Automation_PowerShell_3.0
-     * 
-     */ 
     [Cmdlet("Get", "Stupider")]
     class GetStupider : PSCmdlet
     {
